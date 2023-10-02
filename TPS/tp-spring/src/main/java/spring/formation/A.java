bea2e5583932871a349f730cdfd4da1c18e375d8
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2023-10-02 11:41:54 +0200

    Initial Commit

---
+++ TPS/tp-spring/src/main/java/spring/formation/A.java
@@ -0,0 +1,19 @@
+package spring.formation;
+
+public class A {
+	private String rue;
+
+	public A() {
+		super();
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	
+	
+}
---
+++ TPS/tp-spring/src/main/java/spring/formation/B.java
@@ -0,0 +1,19 @@
+package spring.formation;
+
+public class B {
+	private A monA;
+
+	public B() {
+		super();
+	}
+
+	public A getMonA() {
+		return monA;
+	}
+
+	public void setMonA(A monA) {
+		this.monA = monA;
+	}
+	
+	
+}
