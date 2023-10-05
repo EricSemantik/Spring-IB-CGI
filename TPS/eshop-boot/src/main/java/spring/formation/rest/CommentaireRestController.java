a15d57fa4c7873293110ef8450b71b5a47f0e835
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2023-10-05 14:03:51 +0200

    modifi rest controller commentaire

--- TPS/eshop-boot/src/main/java/spring/formation/rest/CommentaireRestController.java
+++ TPS/eshop-boot/src/main/java/spring/formation/rest/CommentaireRestController.java
@@ -1,10 +1,19 @@
 package spring.formation.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -13,15 +22,57 @@ import spring.formation.model.Views;
 import spring.formation.repo.ICommentaireRepository;
 
 @RestController
+@RequestMapping("/api/commentaire")
 public class CommentaireRestController {
 
 	@Autowired
 	private ICommentaireRepository commentaireRepository;
 	
-	@GetMapping("/api/commentaire")
+	@GetMapping("")
 	@JsonView(Views.ViewCommentaire.class)
 	public List<Commentaire> findAll() {
 		return commentaireRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	@JsonView(Views.ViewCommentaire.class)
+	public Commentaire findById(@PathVariable Long id) {
+		Optional<Commentaire> optCommentaire = commentaireRepository.findById(id);
+		
+		if(optCommentaire.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		
+		return optCommentaire.get();
+	}
+	
+	@PostMapping("")
+	@JsonView(Views.ViewCommentaire.class)
+	public Commentaire create(@RequestBody Commentaire commentaire) {
+		commentaire =  commentaireRepository.save(commentaire);
+		
+		return commentaire;
+	}
+	
+	@PutMapping("/{id}")
+	@JsonView(Views.ViewCommentaire.class)
+	public Commentaire update(@RequestBody Commentaire commentaire, @PathVariable Long id) {
+		if(id != commentaire.getId() || !commentaireRepository.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		
+		commentaire =  commentaireRepository.save(commentaire);
+		
+		return commentaire;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		if(!commentaireRepository.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		
+		commentaireRepository.deleteById(id);
 	}
 	
 }
